3bb3894332211738735b1dc681c9303680855f4b
shashikanuwan <kumararanaweera1999@gmail.com>
2025-02-05 23:05:18 +0530

    Create Post Model

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\PostFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    /** @use HasFactory<PostFactory> */
+    use HasFactory;
+
+    public function website(): BelongsTo
+    {
+        return $this->belongsTo(Website::class);
+    }
+}
--- app/Models/Website.php
+++ app/Models/Website.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Database\Factories\WebsiteFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Website extends Model
 {
     /** @use HasFactory<WebsiteFactory> */
     use HasFactory;
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
