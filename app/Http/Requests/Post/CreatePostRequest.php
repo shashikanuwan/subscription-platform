e0e82eedb98599986c3893a9ccf17fcd5e4c69fd
shashikanuwan <kumararanaweera1999@gmail.com>
2025-02-06 15:03:39 +0530

    feat(api, test): add CreatePostController

---
+++ app/Http/Requests/Post/CreatePostRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreatePostRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|min:5|max:50',
+            'description' => 'required|max:255',
+            'website_id' => 'required|exists:websites,id',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Post\CreatePostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('posts', CreatePostController::class)
+    ->name('posts.store');
